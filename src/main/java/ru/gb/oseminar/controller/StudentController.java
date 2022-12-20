2308d5ab3783694d51281785d4bc422c70f639a4
aksdar31415@gmail.com <Zx38745!!!!!>
2022-12-20 14:50:32 +0300

    fix StudentController.java

--- src/main/java/ru/gb/oseminar/controller/StudentController.java
+++ src/main/java/ru/gb/oseminar/controller/StudentController.java
@@ -4,33 +4,41 @@ import ru.gb.oseminar.data.Student;
 import ru.gb.oseminar.data.Teacher;
 import ru.gb.oseminar.service.StudentGroupService;
 import ru.gb.oseminar.service.StudentService;
+import ru.gb.oseminar.view.StudentView;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 public class StudentController implements UserController<Student>{
 
     private final StudentService dataService = new StudentService();
     private final StudentGroupService studentGroupService = new StudentGroupService();
+    private final StudentView studentView = new StudentView();
 
     @Override
     public void create(String firstName, String secondName, String patronymic, LocalDate dateOfBirth) {
         dataService.create(firstName, secondName, patronymic, dateOfBirth);
+        studentView.sendOnConsole(dataService.getAll());
     }
 
     public void createStudentGroup(Teacher teacher, List<Student> students){
         studentGroupService.createStudentGroup(teacher, students);
+        studentView.sendOnConsoleUserGroup(studentGroupService.getStudentGroup());
     }
 
-    public Student getStudentInStudentGroup(String firstName, String secondName){
-        return studentGroupService.getStudentFromStudentGroup(firstName, secondName);
+    public void getStudentInStudentGroup(String firstName, String secondName){
+        Student student = studentGroupService.getStudentFromStudentGroup(firstName, secondName);
+        studentView.sendOnConsole(Collections.singletonList(student));
     }
 
-    public List<Student> getSortedListStudentFromStudentGroup(){
-        return studentGroupService.getSortedStudentGroup();
+    public void getSortedListStudentFromStudentGroup(){
+        List<Student> students = studentGroupService.getSortedStudentGroup();
+        studentView.sendOnConsole(students);
     }
 
-    public List<Student> getSortedListByFIOStudentFromStudentGroup(){
-        return studentGroupService.getSortedByFIOStudentGroup();
+    public void getSortedListByFIOStudentFromStudentGroup(){
+        List<Student> students = studentGroupService.getSortedByFIOStudentGroup();
+        studentView.sendOnConsole(students);
     }
 }
